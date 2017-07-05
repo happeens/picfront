e5022d654e94fa9dd05a83d2e4c5eef8c4240985
Hilmar Wiegand <hilmar.wiegand@flameshome.net>
2017-07-05 23:28:32 +0200

    :sparkles: Implement buffer

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -1,6 +1,11 @@
 <template>
-<div class="container">
-    <img :src="getCurrentUrl()">
+<div>
+    <div class="container">
+        <img :src="getCurrentUrl()">
+    </div>
+    <div class="buffer">
+        <div v-for="picture in buffer" :key="picture" :style="getBufferStyle(picture)"></div>
+    </div>
 </div>
 </template>
 
@@ -38,6 +43,9 @@ export default {
             if (!this.current) return ''
 
             return 'http://localhost:8000' + this.current.filename
+        },
+        getBufferStyle: function(picture) {
+            return 'background-image: url(http://localhost:8000' + picture.filename + ');'
         }
     }
 }
@@ -45,13 +53,20 @@ export default {
 
 <style lang="scss" scoped>
 div.container {
+    overflow: hidden;
     width: 100%;
-    height: 100vw;
+    height: 100vh;
 
     img {
+        height: 100%;
         width: 100%;
-        height: 100vw;
         object-fit: cover;
     }
+}
+
+div.buffer {
+    width: 0px;
+    height: 0px;
+    overflow: hidden;
 }
 </style>
